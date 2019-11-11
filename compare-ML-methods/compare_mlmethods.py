64cf78313dbf0844315b06234985a05dec780eec
gilad <gilad.braunschvig@samsungnext.com>
2019-11-11 11:49:41 +0200

    add interactive mode for model comarison format

--- compare-ML-methods/compare_mlmethods.py
+++ compare-ML-methods/compare_mlmethods.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 from sklearn import model_selection
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
@@ -9,78 +8,239 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
+from sklearn.neural_network import MLPClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+import random
+import time
+from optparse import OptionParser
+
+data_sets = {
+    "s": {
+        "link": "https://s3-us-west-2.amazonaws.com/iqoqo.temp/demo/all_train_10000.csv",
+        "size": 10000,
+    },
+    "m": {
+        "link": "https://s3-us-west-2.amazonaws.com/iqoqo.temp/demo/all_train_30000.csv",
+        "size": 30000,
+    },
+    "l": {
+        "link": "https://s3-us-west-2.amazonaws.com/iqoqo.temp/demo/all_train_50000.csv",
+        "size": 50000,
+    },
+    "all": {
+        "link": "https://s3-us-west-2.amazonaws.com/iqoqo.temp/demo/all_train.csv",
+        "size": "5GB",
+    },
+}
 
 
-### define the models pool
-modelnames = ["LogReg", "SVM", "DecTree", "KNN", "LinDisc", "GaussianNB"]
+model_dicts = {
+    "LogReg": {"model": LogisticRegression, "params": {}},
+    "SVM": {"model": SVC, "params": {}},
+    "DecTree": {"model": DecisionTreeClassifier, "params": {}},
+    "KNN": {"model": KNeighborsClassifier, "params": {}},
+    "LinDisc": {"model": LinearDiscriminantAnalysis, "params": {}},
+    "GaussianNB": {"model": GaussianNB, "params": {}},
+    "MLP": {"model": MLPClassifier, "params": {}},
+    "GaussianPC": {"model": GaussianProcessClassifier, "params": {}},
+    "RandomForest": {"model": RandomForestClassifier, "params": {}},
+    "AdaBoost": {"model": AdaBoostClassifier, "params": {}},
+    "QuadraticDisc": {"model": QuadraticDiscriminantAnalysis, "params": {}},
+    "SVClinear": {"model": SVC, "params": {"kernel": "linear", "C": 0.025}},
+    "SVCgamma": {"model": SVC, "params": {"gamma": 2, "C": 1}},
+    "KNN3": {"model": KNeighborsClassifier, "params": {"n_neighbors": 3}},
+    "GaussianRBF": {
+        "model": GaussianProcessClassifier,
+        "params": {"kernel": 1.0 * RBF(1.0)},
+    },
+    "DecTreeDepth": {"model": DecisionTreeClassifier, "params": {"max_depth": 5}},
+    "RandomForestDepth": {
+        "model": RandomForestClassifier,
+        "params": {"max_depth": 100, "n_estimators": 10, "max_features": 1},
+    },
+    "MLPalpha": {"model": MLPClassifier, "params": {"alpha": 1}},
+}
 
-random_seed = 12
+
+def model_str(model_name):
+    cls = model_dicts[model_name]["model"]
+    params = model_dicts[model_name]["params"]
+    return f"{cls.__name__}({params})"
 
 
-def analyse(model_name):
-    ### read in the data (HEP)
-    data = pd.read_csv('https://s3-us-west-2.amazonaws.com/iqoqo.temp/demo/all_train_30000.csv')
-    data.head()
-    data_to_use = data
-    data_to_use.dropna(inplace=True)
-    data_to_use.head()
-    values = data_to_use.values
-    Y = values[:,0]
-    X = values[:,1:28]
-
-
-    print ("--- starting "+model_name+" analysis ---")
-    model = []
-    if(model_name=='LogReg'):       model.append( LogisticRegression() )
-    elif(model_name=='SVM'):        model.append( SVC() )
-    elif(model_name=='DecTree'):    model.append( DecisionTreeClassifier() )
-    elif(model_name=='KNN'):        model.append( KNeighborsClassifier() )
-    elif(model_name=='LinDisc'):    model.append( LinearDiscriminantAnalysis() )
-    elif(model_name=='GaussianNB'): model.append( GaussianNB() )
+def get_model(model_name):
+    if model_name in model_dicts.keys():
+        cls = model_dicts[model_name]["model"]
+        params = model_dicts[model_name]["params"]
+        print(model_str(model_name))
+        model = cls(**params)
     else:
-        print( "Model name not found: "+model_name)
-        quit()
+        print("Model name not found: " + model_name)
+        model = None
 
-    k_fold_validation = model_selection.KFold(n_splits=10, random_state=random_seed)
-    results = model_selection.cross_val_score(model[0], X, Y, cv=k_fold_validation, scoring='accuracy')
-    output_message = "%s| Mean=%f STD=%f" % (model_name, results.mean(), results.std())
-    print(output_message)
-    print ("--- done "+model_name+" analysis ---")
-    return model_name, results
+    return model
 
 
-def linear(imodel=-1):
-    if(imodel<0):
-       results = []
-       names = []
-       for modelname in modelnames:
-          name, result = analyse(modelname)
-          results.append(result)
-          names.append(name)
+def print_results(model_name, results, elapsed):
+    if len(results) == 0:
+        print(f"{model_name}| Skipped")
     else:
-       analyse(modelnames[imodel])
-
-### call linear(...) with integer index to run a specific
-### model, or with no arguments to run all sequentially
-### comment out if you run through multiprocess manager
-# linear()
-
-###########################################################
-#################DISCOmp################################## 
-##########################################################
-import os
-from discomp import Pool
-# from multiprocessing.dummy import Pool
+        print(
+            f"{model_name} [{elapsed} sec]| Mean={results.mean()} STD={results.std()}"
+        )
 
 
-os.environ['DISCO_LOGIN_USER'] = 'user@somthing.com'
-os.environ['DISCO_LOGIN_PASSWORD'] = 'userpassword'
+def analyse(model_name, dset="s"):
+    t = time.time()
+    try:
+        random_seed = random.randint
+        # read in the data (HEP)
 
-p = Pool()
-collectedoutput = p.map(analyse, modelnames)
+        print(f"{model_name} {dset}")
+        print(f"--- read data set {data_sets[dset]} ---")
+
+        data = pd.read_csv(data_sets[dset]["link"])
+        data.dropna(inplace=True)
+        elapsed = time.time() - t
+
+        print(f"--- done reading data set elapsed time {elapsed} ---")
+
+        values = data.values
+        Y = values[:, 0]
+        X = values[:, 1:28]
+
+        model = get_model(model_name)
+        if model is None:
+            return
+
+        t = time.time()
+
+        print(f"--- starting {model_name} analysis ---")
+
+        k_fold_validation = model_selection.KFold(n_splits=10, random_state=random_seed)
+        results = model_selection.cross_val_score(
+            model, X, Y, cv=k_fold_validation, scoring="accuracy"
+        )
+
+        elapsed = time.time() - t
+        print(f"--- done {model_name} analysis elapsed time {elapsed} ---")
+
+        return model_name, results, elapsed
+    except Exception as e:
+        print(e)
+        elapsed = time.time() - t
+        return model_name, [], elapsed
 
 
-###########################################################
-#################DISCOmp################################## 
-##########################################################
+def gen_pool_params(model_names, dset):
+    return [[m, dset] for m in model_names]
 
+
+def linear_main(options):
+    collected_output = []
+    for m in options.model_names:
+        collected_output.append(analyse(m, options.data_set_size))
+    return collected_output
+
+
+def mp_main(options):
+    from multiprocessing import Pool
+
+    p = Pool(processes=4)
+    collected_output = p.starmap(
+        analyse, gen_pool_params(options.model_names, options.data_set_size)
+    )
+    return collected_output
+
+
+def discomp_main(options):
+    import os
+    from discomp import Pool
+
+    p = Pool()
+    collected_output = p.starmap(
+        analyse, gen_pool_params(options.model_names, options.data_set_size)
+    )
+    return collected_output
+
+
+execs = {
+    "disco": {"ex": discomp_main, "desc": "disco cloud",},
+    "linear": {"ex": linear_main, "desc": "sequentially local",},
+    "multi_process": {"ex": mp_main, "desc": "multi-process local",},
+}
+
+
+def opts():
+
+    parser = OptionParser()
+    models_help = (
+        """Specify model(s) to train. May specify more than one: """
+        + ", ".join(f"{model_name}" for model_name in model_dicts.keys())
+    )
+
+    parser.add_option(
+        "-m",
+        "--model",
+        action="append",
+        dest="model_names",
+        default=[],
+        help=models_help,
+    )
+
+    parser.add_option(
+        "-a",
+        "--all_models",
+        action="store_true",
+        dest="all_models",
+        default=False,
+        help="evaluate all models. overrides '-m' option",
+    )
+
+    exec_help = "choose execution mode: " + ", ".join(
+        f"'{k}' ({v['desc']})" for k, v in execs.items()
+    )
+
+    parser.add_option(
+        "-e", "--exec_mode", dest="exec_mode", default="linear", help=exec_help
+    )
+
+    help_data_set = "choose data set: " + ", ".join(
+        f"'{k}' ({v['size']})" for k, v in data_sets.items()
+    )
+    parser.add_option(
+        "-d", "--data_set_size", dest="data_set_size", default="s", help=help_data_set
+    )
+
+    options, _ = parser.parse_args()
+
+    if options.all_models:
+        options.model_names = model_dicts.keys()
+    options.model_names = set(options.model_names)
+
+    return options
+
+
+def main():
+    options = opts()
+    print(
+        f"Welcome to model comparison demo\n"
+        f"Going to run in execution mode {options.exec_mode}\n"
+        f"Using data set {options.data_set_size} "
+        f"with {data_sets[options.data_set_size]['size']} entries"
+    )
+    print(f"Evaluating {len(options.model_names)} models: ")
+    for model_name in options.model_names:
+        print(f"\t{model_name}: {model_str(model_name)}")
+
+    ex = execs[options.exec_mode]["ex"]
+    collected_output = ex(options)
+    for model, result, elapsed in collected_output:
+        print_results(model, result, elapsed)
+
+
+if __name__ == "__main__":
+    main()
--- compare-ML-methods/compare_mlmethods_18.py
+++ compare-ML-methods/compare_mlmethods_18.py
@@ -15,72 +15,115 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
 
-
-
 ### define the models pool
-modelnames = ["LogReg", "SVM", "DecTree", "KNN", "LinDisc", "GaussianNB",
-              "MLP", "GaussianPC", "RandomForest", "AdaBoost", "QuadraticDisc",
-              "SVClinear", "SVCgamma", "KNN3", "GaussianRBF", "DecTreeDepth", "RandomForestDepth", "MLPalpha"]
+modelnames = [
+    "LogReg",
+    "LogReg",
+    "SVM",
+    "DecTree",
+    "KNN",
+    "LinDisc",
+    "MLP",
+    "GaussianPC",
+    "AdaBoost",
+    "QuadraticDisc",
+    "SVClinear",
+    "KNN3",
+    "GaussianRBF",
+    "DecTreeDepth",
+    "RandomForestDepth",
+    "MLPalpha",
+]
 
 random_seed = 12
 
 
 def analyse(model_name):
-    ### read in the data (HEP)
-    data = pd.read_csv('https://s3-us-west-2.amazonaws.com/iqoqo.temp/demo/all_train_10000.csv')
-    data.head()
-    data_to_use = data
-    data_to_use.dropna(inplace=True)
-    data_to_use.head()
-    values = data_to_use.values
-    Y = values[:,0]
-    X = values[:,1:28]
-    
-    
-    print ("--- starting "+model_name+" analysis ---")
-    model = []
-    if(model_name=='LogReg'):              model.append( LogisticRegression() )
-    elif(model_name=='SVM'):               model.append( SVC() )
-    elif(model_name=='DecTree'):           model.append( DecisionTreeClassifier() )
-    elif(model_name=='KNN'):               model.append( KNeighborsClassifier() )
-    elif(model_name=='LinDisc'):           model.append( LinearDiscriminantAnalysis() )
-    elif(model_name=='GaussianNB'):        model.append( GaussianNB() )
-    elif(model_name=='MLP'):               model.append( MLPClassifier() )
-    elif(model_name=='GaussianPC'):        model.append( GaussianProcessClassifier() )
-    elif(model_name=='RandomForest'):      model.append( RandomForestClassifier() )
-    elif(model_name=='AdaBoost'):          model.append( AdaBoostClassifier() )
-    elif(model_name=='QuadraticDisc'):     model.append( QuadraticDiscriminantAnalysis() )
-    elif(model_name=='SVClinear'):         model.append( SVC(kernel="linear", C=0.025) )
-    elif(model_name=='SVCgamma'):          model.append( SVC(gamma=2, C=1) )
-    elif(model_name=='KNN3'):              model.append( KNeighborsClassifier(3) )
-    elif(model_name=='GaussianRBF'):       model.append( GaussianProcessClassifier(1.0 * RBF(1.0)) )
-    elif(model_name=='DecTreeDepth'):      model.append( DecisionTreeClassifier(max_depth=5) )
-    elif(model_name=='RandomForestDepth'): model.append( RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1) )
-    elif(model_name=='MLPalpha'):          model.append( MLPClassifier(alpha=1) )
-    else:
-        print ("Model name not found: "+model_name)
-        quit()
+    try:
+        ### read in the data (HEP)
+        data = pd.read_csv(
+            "https://s3-us-west-2.amazonaws.com/iqoqo.temp/demo/all_train_10000.csv"
+        )
+        data.head()
+        data_to_use = data
+        data_to_use.dropna(inplace=True)
+        data_to_use.head()
+        values = data_to_use.values
+        Y = values[:, 0]
+        X = values[:, 1:28]
 
-    
-    k_fold_validation = model_selection.KFold(n_splits=10, random_state=random_seed)
-    results = model_selection.cross_val_score(model[0], X, Y, cv=k_fold_validation, scoring='accuracy')
-    output_message = "%s| Mean=%f STD=%f" % (model_name, results.mean(), results.std())
-    print(output_message)
-    print ("--- done "+model_name+" analysis ---")
-    return model_name, results
+        print("--- starting " + model_name + " analysis ---")
+        model = []
+        if model_name == "LogReg":
+            model.append(LogisticRegression())
+        elif model_name == "SVM":
+            model.append(SVC())
+        elif model_name == "DecTree":
+            model.append(DecisionTreeClassifier())
+        elif model_name == "KNN":
+            model.append(KNeighborsClassifier())
+        elif model_name == "LinDisc":
+            model.append(LinearDiscriminantAnalysis())
+        elif model_name == "GaussianNB":
+            model.append(GaussianNB())
+        elif model_name == "MLP":
+            model.append(MLPClassifier())
+        elif model_name == "GaussianPC":
+            model.append(GaussianProcessClassifier())
+        elif model_name == "RandomForest":
+            model.append(RandomForestClassifier())
+        elif model_name == "AdaBoost":
+            model.append(AdaBoostClassifier())
+        elif model_name == "QuadraticDisc":
+            model.append(QuadraticDiscriminantAnalysis())
+        elif model_name == "SVClinear":
+            model.append(SVC(kernel="linear", C=0.025))
+        elif model_name == "SVCgamma":
+            model.append(SVC(gamma=2, C=1))
+        elif model_name == "KNN3":
+            model.append(KNeighborsClassifier(3))
+        elif model_name == "GaussianRBF":
+            model.append(GaussianProcessClassifier(1.0 * RBF(1.0)))
+        elif model_name == "DecTreeDepth":
+            model.append(DecisionTreeClassifier(max_depth=5))
+        elif model_name == "RandomForestDepth":
+            model.append(
+                RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1)
+            )
+        elif model_name == "MLPalpha":
+            model.append(MLPClassifier(alpha=1))
+        else:
+            print("Model name not found: " + model_name)
+            quit()
+
+        k_fold_validation = model_selection.KFold(n_splits=10, random_state=random_seed)
+        results = model_selection.cross_val_score(
+            model[0], X, Y, cv=k_fold_validation, scoring="accuracy"
+        )
+        output_message = "%s| Mean=%f STD=%f" % (
+            model_name,
+            results.mean(),
+            results.std(),
+        )
+        print(output_message)
+        print("--- done " + model_name + " analysis ---")
+        return model_name, results
+    except:
+        return model_name, []
 
 
 def linear(imodel=-1):
-    if(imodel<0):
-       results = []
-       names = []
-       for modelname in modelnames:
-          name, result = analyse(modelname)
-          results.append(result)
-          names.append(name)
-       plotsummary(names,results)
+    if imodel < 0:
+        results = []
+        names = []
+        for modelname in modelnames:
+            name, result = analyse(modelname)
+            results.append(result)
+            names.append(name)
+        plotsummary(names, results)
     else:
-       analyse(modelnames[imodel])
+        analyse(modelnames[imodel])
+
 
 ### call linear(...) with integer index to run a specific
 ### model, or with no arguments to run all sequentially
@@ -88,21 +131,18 @@ def linear(imodel=-1):
 # linear()
 
 ###########################################################
-#################DISCOmp################################## 
+#################DISCOmp##################################
 ##########################################################
 import os
 from discomp import Pool
+
 # from multiprocessing.dummy import Pool
 
 
-os.environ['DISCO_LOGIN_USER'] = 'user@somthing.com'
-os.environ['DISCO_LOGIN_PASSWORD'] = 'userpassword'
-
 p = Pool()
 collectedoutput = p.map(analyse, modelnames)
-
+print(collectedoutput)
 
 ###########################################################
-#################DISCOmp################################## 
+#################DISCOmp##################################
 ##########################################################
-
